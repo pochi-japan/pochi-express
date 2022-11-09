c348d9b946f2b3f77b0f242d465f2727c8f1af1b
caterada <caterada@gmail.com>
2022-11-08 22:36:03 -0800

    add progress to check backend token

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,17 +2,36 @@ const express = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const { createUserToken } = require('../middleware/auth');
-
 const router = express.Router();
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // SIGN UP
 // POST /api/signup
+// router.post('/signup', async (req, res, next) => {
+// 	try {
+// 		//default number is 10, the number is how many characters is going to be hashed with the password, 6-10 is normal
+// 		const password = await bcrypt.hash(req.body.password, 8);
+// 		const user = await User.create({
+// 			email: req.body.email,
+// 			password,
+// 			name: req.body.name,
+// 		});
+// 		return res.status(201).json(user);
+// 	} catch (err) {
+// 		return next(err);
+// 	}
+// });
+
+//SIGN UP
 router.post('/signup', async (req, res, next) => {
 	try {
-		//default number is 10, the number is how many characters is going to be hashed with the password, 6-10 is normal
-		const password = await bcrypt.hash(req.body.password, 8);
-		const user = await User.create({ email: req.body.email, password, name: req.body.name });
-		return res.status(201).json(user);
+		const user = await User.create(req.body);
+		// token will be a string
+		const token = createJWT(user);
+		// send back the token as a string
+		// which we need to account for
+		// in the client
+		return res.json(token);
 	} catch (err) {
 		return next(err);
 	}
@@ -24,11 +43,31 @@ router.post('/signin', (req, res, next) => {
 	User.findOne({ email: req.body.email })
 		// Pass the user and the request to createUserToken
 		.then((user) => createUserToken(req, user))
+		// .then((user) => console.log(user))
 		// createUserToken will either throw an error that
 		// will be caught by our error handler or send back
 		// a token that we'll in turn send to the client.
-		.then((token) => res.json({ token }))
+		.then((token) => res.json(token))
 		.catch(next);
 });
+
+function checkToken(req, res) {
+	console.log('req.user', req.user);
+	res.json(req.exp);
+}
+
+// GET /api/users/check-token
+router.get('/check-token', ensureLoggedIn, checkToken);
+
+/*-- Helper Functions --*/
+
+function createJWT(user) {
+	return jwt.sign(
+		// data payload
+		{ user },
+		process.env.SECRET,
+		{ expiresIn: '24h' }
+	);
+}
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('../db/connection');
+const bcrypt = require('bcrypt');
+
+// cost factor
+const SALT_ROUNDS = 6;
 
 const userSchema = new mongoose.Schema(
 	{
@@ -11,6 +15,7 @@ const userSchema = new mongoose.Schema(
 		},
 		password: {
 			type: String,
+			trim: true,
 			required: true,
 		},
 		name: {
@@ -30,6 +35,14 @@ const userSchema = new mongoose.Schema(
 		},
 	}
 );
+
+userSchema.pre('save', async function (next) {
+	// 'this' is the user doc
+	if (!this.isModified('password')) return next();
+	// update the password with the computed hash
+	this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+	return next();
+});
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
